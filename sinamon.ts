b059a5ddd1eecda64933943013df161e52e061a6
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2023-08-24 11:11:35 +0900

    更新 sinamon.ts

--- sinamon.ts
+++ sinamon.ts
@@ -548,7 +548,7 @@ namespace sinamon {
     }
 
 
-    //% color="#ffa500"  weight=16 blockId=color_temp block="color Temperatures value" group="8 color_sensor"
+    //% color="#ffa500"  weight=16 blockId=color_temp block="color Temperatures value" group="8 color sensor"
     //% advanced=true
     export function color_temp(): number {
         return Math.round(3810 * getBlue() / getRed() + 1391)
