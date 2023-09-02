a0ca26b931ca7ae0991a062cb86cb8ae8304987d
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2023-09-02 23:07:17 +0900

    更新 sinamon.ts

--- sinamon.ts
+++ sinamon.ts
@@ -200,7 +200,7 @@ namespace sinamon {
         noservo = 1
         Lmoter = 0
         Rmoter = 0
-        nunber_revolution(step*0.2, step * -0.2)
+        nunber_revolution(step*0.19, step * -0.19)
         L_forward()
         R_backward()
         while (Lmoter == 0 || Rmoter == 0) {
@@ -228,7 +228,7 @@ namespace sinamon {
         noservo = 1
         Lmoter = 0
         Rmoter = 0
-        nunber_revolution(step * -0.2, step*0.2)
+        nunber_revolution(step * -0.19, step*0.19)
         L_backward()
         R_forward()
         while (Lmoter == 0 || Rmoter == 0) {
