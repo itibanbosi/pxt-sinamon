b50520f03937729532f62085256539625c440cd7
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2023-09-03 10:29:06 +0900

    更新 sinamon.ts

--- sinamon.ts
+++ sinamon.ts
@@ -389,7 +389,7 @@ namespace sinamon {
 
         switch (sinkou_houkou) {
             case direction.forward:
-                pins.analogWritePin(AnalogPin.P2, Power * 10.23)
+                pins.analogWritePin(AnalogPin.P2, Power * 9.5)
                 pins.analogWritePin(AnalogPin.P13, 0)
 
                 pins.analogWritePin(AnalogPin.P15, 0)
