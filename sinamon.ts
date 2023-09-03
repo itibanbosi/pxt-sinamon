2c1b662005920963527d924c597c235aca228207
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2023-09-03 10:05:29 +0900

    更新 sinamon.ts

--- sinamon.ts
+++ sinamon.ts
@@ -126,7 +126,7 @@ namespace sinamon {
     //% color="#808080" weight=82 block="forward |%step| step" group="1 sinamon"
     //% step.min=0 step.max=50 
     export function forward(step: number): void {
-        sinamon.car_derection(sinamon.direction.Stop, 0)
+        //sinamon.car_derection(sinamon.direction.Stop, 0)
         basic.pause(300)
         noservo = 1
         Lmoter = 0
@@ -144,7 +144,7 @@ namespace sinamon {
     //% color="#808080" weight=82 block="forward |%step| cm" group="1 sinamon"
     //% step.min=0 step.max=50 
     export function forward_cm(step: number): void {
-        sinamon.car_derection(sinamon.direction.Stop, 0)
+        //sinamon.car_derection(sinamon.direction.Stop, 0)
         basic.pause(300)
         noservo = 1
         Lmoter = 0
@@ -162,7 +162,7 @@ namespace sinamon {
     //% color="#808080" weight=82 block="back |%step| step" group="1 sinamon"
     //% step.min=0 step.max=50 
     export function back(step: number): void {
-        sinamon.car_derection(sinamon.direction.Stop, 0)
+        //sinamon.car_derection(sinamon.direction.Stop, 0)
         basic.pause(300)
         noservo = 1
         Lmoter = 0
@@ -179,7 +179,7 @@ namespace sinamon {
     //% color="#808080" weight=82 block="back |%step| step" group="1 sinamon"
     //% step.min=0 step.max=50 
     export function back_cm(step: number): void {
-        sinamon.car_derection(sinamon.direction.Stop, 0)
+        //sinamon.car_derection(sinamon.direction.Stop, 0)
         basic.pause(300)
         noservo = 1
         Lmoter = 0
@@ -197,7 +197,7 @@ namespace sinamon {
     //% color="#808080" weight=80 block="right |%step| step" group="1 sinamon"
     //% step.min=0 step.max=50 
     export function right(step: number): void {
-        sinamon.car_derection(sinamon.direction.Stop, 0)
+        //sinamon.car_derection(sinamon.direction.Stop, 0)
         basic.pause(300)
         noservo = 1
         Lmoter = 0
@@ -214,7 +214,7 @@ namespace sinamon {
     //% color="#808080" weight=80 block="right |%step|degree" group="1 sinamon"
     //% step.min=0 step.max=50 
     export function right_degree(step: number): void {
-        sinamon.car_derection(sinamon.direction.Stop, 0)
+        //sinamon.car_derection(sinamon.direction.Stop, 0)
         basic.pause(300)
         noservo = 1
         Lmoter = 0
@@ -231,7 +231,7 @@ namespace sinamon {
     //% color="#808080" weight=80 block="left |%step| step" group="1 sinamon"
     //% step.min=0 step.max=50 
     export function left(step: number): void {
-        sinamon.car_derection(sinamon.direction.Stop, 0)
+        //sinamon.car_derection(sinamon.direction.Stop, 0)
         basic.pause(300)
         noservo = 1
         Lmoter = 0
@@ -248,7 +248,7 @@ namespace sinamon {
     //% color="#808080" weight=80 block="left |%step| degree" group="1 sinamon"
     //% step.min=0 step.max=50 
     export function left_degree(step: number): void {
-        sinamon.car_derection(sinamon.direction.Stop, 0)
+        //sinamon.car_derection(sinamon.direction.Stop, 0)
         basic.pause(300)
         noservo = 1
         Lmoter = 0
