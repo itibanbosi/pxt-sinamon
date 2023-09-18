cf65094774a7724a7f1a8ddcbaacd86e3688611b
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2023-09-18 11:27:42 +0900

    更新 sinamon.ts

--- sinamon.ts
+++ sinamon.ts
@@ -330,7 +330,7 @@ namespace sinamon {
                 if (P1count + 10 < Math.abs(objectP1)) {
                     R_power = 300
                     if (P0count > P1count) {
-                        R_power = 400
+                        R_power = 60
                     }
                     if (objectP1 == Math.abs(objectP1)) {
                         R_forward()
@@ -367,7 +367,7 @@ namespace sinamon {
                 if (P0count + 10 < Math.abs(objectP0)) {
                     L_Power = 300
                     if (P0count < P1count) {
-                        L_Power = 400
+                        L_Power = 60
                     }
                     if (objectP0 == Math.abs(objectP0)) {
                         L_forward()
